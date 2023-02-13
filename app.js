5ad763c2dc403903f791318aa3a4f6e2a06945bc
Zeyad_Attia <85710632+ZeyadAttia5@users.noreply.github.com>
2023-02-13 20:21:37 +0200

    Added and styled the form, added the 'show' functionality to the Button

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 let myLibrary = [];
 
-function Book() {
-  // the constructor...
+function Book(title, author, dateOfPublication) {
+  this.title = title;
+  this.author = author;
+  this.dateOfPublication = dateOfPublication;
 }
 
 function addBookToLibrary() {
-  // do stuff here
+  let book = new Book();
+  book.title = prompt('What is the name of the book?');
+  book.author = prompt('Who is the author of the book?');
+  book.dateOfPublication = prompt('When was the book published?');
+  myLibrary.push(book);
+}
+
+function printLibrary() {
+  myLibrary.forEach((book) => console.log(book));
+}
+
+const newBookBtn = document.querySelector('.newbook');
+
+newBookBtn.addEventListener('click', newBookHandler);
+
+function newBookHandler() {
+  document.getElementById('myForm').style.display = 'none';
 }
