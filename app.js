faa39a81ac0a64ffa933e0521b6edb159ba103e6
Zeyad_Attia <85710632+ZeyadAttia5@users.noreply.github.com>
2023-02-13 20:32:02 +0200

    The cancel button now hides the form

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ function Book(title, author, dateOfPublication) {
 
 function addBookToLibrary() {
   let book = new Book();
-  book.title = prompt('What is the name of the book?');
-  book.author = prompt('Who is the author of the book?');
-  book.dateOfPublication = prompt('When was the book published?');
+  book.title = prompt("What is the name of the book?");
+  book.author = prompt("Who is the author of the book?");
+  book.dateOfPublication = prompt("When was the book published?");
   myLibrary.push(book);
 }
 
@@ -18,10 +18,16 @@ function printLibrary() {
   myLibrary.forEach((book) => console.log(book));
 }
 
-const newBookBtn = document.querySelector('.newbook');
+const newBookBtn = document.querySelector(".newbook");
+const cancelBtn = document.querySelector("#cancelBtn");
 
-newBookBtn.addEventListener('click', newBookHandler);
+newBookBtn.addEventListener("click", newBookHandler);
+cancelBtn.addEventListener("click", closeBookHandler);
 
 function newBookHandler() {
-  document.getElementById('myForm').style.display = 'none';
+  document.getElementById("myForm").style.display = "flex";
+}
+
+function closeBookHandler() {
+  document.getElementById("myForm").style.display = "none";
 }
