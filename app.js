461f8e6703e310d9d48ac995121db21b92c97d4f
Zeyad_Attia <85710632+ZeyadAttia5@users.noreply.github.com>
2023-02-14 20:40:54 +0200

    users can now add a book to the library

--- app.js
+++ app.js
@@ -1,35 +1,78 @@
-let myLibrary = [];
+const myLibrary = [];
 
-function Book(title, author, dateOfPublication) {
+function Book(title, author, pages, dateOfPublication) {
   this.title = title;
   this.author = author;
+  this.pages = pages;
   this.dateOfPublication = dateOfPublication;
+  this.read = false;
 }
 
 function addBookToLibrary() {
   const book = new Book();
-  book.title = prompt('What is the name of the book?');
-  book.author = prompt('Who is the author of the book?');
-  book.dateOfPublication = prompt('When was the book published?');
-  myLibrary.push(book);
+  const formBtn = document.querySelector('#submission');
+  const form = document.querySelector('form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+  });
+  formBtn.addEventListener('click', submissionHandler);
+  function submissionHandler(e) {
+    e.preventDefault();
+    book.title = form.querySelector('.title').querySelector('input').value;
+    book.author = form.querySelector('.author').querySelector('input').value;
+    book.pages = `${form.querySelector('.pages').querySelector('input').value} pages`;
+    book.dateOfPublication = form.querySelector('.year').querySelector('input').value;
+    myLibrary.push(book);
+    printLibrary(book);
+    closeFormHandler();
+  }
 }
 
-function printLibrary() {
-  myLibrary.forEach((book) => console.log(book));
+function printLibrary(book) {
+  const cardContainer = document.querySelector('.card-container');
+  const cardContent = document.createElement('div');
+  cardContent.classList.add('card');
+  const title = document.createElement('p');
+  const author = document.createElement('p');
+  const pages = document.createElement('p');
+  const year = document.createElement('p');
+
+  title.textContent = book.title;
+  author.textContent = book.author;
+  pages.textContent = book.pages;
+  year.textContent = book.dateOfPublication;
+
+  const cardBtns = document.createElement('div');
+  const readBtn = document.createElement('btn');
+  const cancelBtn = document.createElement('btn');
+  readBtn.textContent = 'read';
+  cancelBtn.textContent = 'delete';
+  readBtn.classList.add('read', 'btn');
+  cancelBtn.classList.add('delete');
+  cardBtns.classList.add('card-buttons');
+  cardBtns.append(readBtn);
+  cardBtns.append(cancelBtn);
+  cardContent.append(title);
+  cardContent.append(author);
+  cardContent.append(pages);
+  cardContent.append(year);
+  cardContent.append(cardBtns);
+  cardContainer.append(cardContent);
 }
 
 const newBookBtn = document.querySelector('.newbook');
 const cancelBtn = document.querySelector('#cancelBtn');
 
 newBookBtn.addEventListener('click', newBookHandler);
-cancelBtn.addEventListener('click', closeBookHandler);
+cancelBtn.addEventListener('click', closeFormHandler);
 
 function newBookHandler() {
   document.getElementById('myForm').style.display = 'block';
   document.getElementById('overlay').style.display = 'block';
+  addBookToLibrary();
 }
 
-function closeBookHandler() {
+function closeFormHandler() {
   document.getElementById('myForm').style.display = 'none';
   document.getElementById('overlay').style.display = 'none';
 }
